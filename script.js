6a1982a21537e88dd550047c8bd2b6d22a2b77dd
jordanx2 <jordanmooney9@gmail.com>
2024-10-12 19:14:56 +0100

    ...

--- script.js
+++ script.js
@@ -1,9 +1,32 @@
-import { createTable } from '/shared/utils.js';
-
 let fileNames = [];
 const referenceFile = 'reference.json';
 const endpoint = 'https://jordanx2.github.io/rwat-lab4/data/';
 const tableHeadings = ['name', 'id', 'address', 'grades'];
+
+export const createTable = (tableData, tableHeadings) => {
+  if(!tableData || !tableHeadings) return;
+
+  const table = document.createElement('table');
+  const headingRow = document.createElement('tr');
+  document.body.appendChild(table); 
+  table.append(headingRow)
+  
+  tableHeadings.map(item => {
+    let header = document.createElement('th');
+    header.append(item);
+    headingRow.append(header);
+  });
+  
+  tableData.map(student => {
+    const dataRow = document.createElement('tr');
+    tableHeadings.map(data => {
+      const dataItem = document.createElement('td');
+      dataItem.append(student[data])
+      dataRow.append(dataItem)
+    })
+    table.append(dataRow);
+  });
+};
 
 const populateFileNames = async (fileName, finalAppend, result = []) => {
   const res = await fetch(`${endpoint}${fileName}`);
